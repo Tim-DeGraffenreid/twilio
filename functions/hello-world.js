84f87ddb7785fa66d2366df46c5c77bb6ae09c71
Tim-DeGraffenreid <timd061677@gmail.com>
2024-04-25 03:35:25 +0000

    add

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 exports.handler = function(context, event, callback) {
+  
+
+
   const twiml = new Twilio.twiml.VoiceResponse();
-  console.log(process.env);
+  
+  console.log(process.env.MY_SECRET);
   twiml.say('Hello World!');
   callback(null, twiml);
 };
